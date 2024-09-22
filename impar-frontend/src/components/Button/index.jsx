e5668a1b0927c38fa606fdf9519fcc2ef34d82b7
Pablo Ferreira <pabloferreirajc@gmail.com>
2024-09-22 16:41:17 -0300

    first commit

---
+++ impar-frontend/src/components/Button/index.jsx
@@ -0,0 +1,11 @@
+import { ButtonDefault } from './styles.js'
+
+function Button(){
+    return (
+        <div>
+            <ButtonDefault>Novo Card</ButtonDefault>
+        </div>
+    )
+}
+
+export default Button
---
+++ impar-frontend/src/components/Button/styles.js
@@ -0,0 +1,10 @@
+
+import styled from "styled-components";
+
+export const ButtonDefault = styled.button `
+
+    color: white;
+    background-color: orange;
+    width: 200px;
+    height: 100px;
+`
